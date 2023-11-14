a1f7ca0626e533d8e8eaff39b52bb0737918c189
Sprawl2387 <scraggly5717@gmail.com>
2023-11-14 17:01:16 +0530

    latest addition in controller

--- eYRC-2023_Hologlyph_Bots/hb_task2a/scripts/controller.py
+++ eYRC-2023_Hologlyph_Bots/hb_task2a/scripts/controller.py
@@ -25,8 +25,7 @@
 #			[ Comma separated list of functions in this file ]
 # Nodes:		Add your publishing and subscribing node
 
-
-################### IMPORT MODULES #######################
+#! /usr/bin/env python3
 
 import rclpy
 from rclpy.node import Node
@@ -35,116 +34,121 @@ from nav_msgs.msg import Odometry
 import time
 import math
 from tf_transformations import euler_from_quaternion
-from my_robot_interfaces.srv import NextGoal             
+from my_robot_interfaces.srv import NextGoal
 
-# You can add more if required
-##############################################################
+class Pose:
+    def __init__(self, x, y, theta):
+        self.x = x
+        self.y = y
+        self.theta = theta
 
-
-# Initialize Global variables
-
-
-################# ADD UTILITY FUNCTIONS HERE #################
-
-##############################################################
-
-
-# Define the HBController class, which is a ROS node
 class HBController(Node):
     def __init__(self):
         super().__init__('hb_controller')
-        
-        # Initialze Publisher and Subscriber
-        # NOTE: You are strictly NOT-ALLOWED to use "cmd_vel" or "odom" topics in this task
-	    #	Use the below given topics to generate motion for the robot.
-	    #   /hb_bot_1/left_wheel_force,
-	    #   /hb_bot_1/right_wheel_force,
-	    #   /hb_bot_1/left_wheel_force
 
+        self.left_wheel_force_pub = self.create_publisher(Twist, '/hb_bot_1/left_wheel_force', 10)
+        self.right_wheel_force_pub = self.create_publisher(Twist, '/hb_bot_1/right_wheel_force', 10)
+        self.back_wheel_force_pub = self.create_publisher(Twist, '/hb_bot_1/back_wheel_force', 10)
 
-
-
-        # For maintaining control loop rate.
         self.rate = self.create_rate(100)
-
-
-        # client for the "next_goal" service
         self.cli = self.create_client(NextGoal, 'next_goal')      
         self.req = NextGoal.Request() 
         self.index = 0
 
-    
-    # Method to create a request to the "next_goal" service
     def send_request(self, request_goal):
         self.req.request_goal = request_goal
         self.future = self.cli.call_async(self.req)
-        
 
-    def inverse_kinematics():
-        ############ ADD YOUR CODE HERE ############
+    def inverse_kinematics(self, target_velocity):
+        # Assume a simple proportional control for inverse kinematics
+        left_force = 0.5 * target_velocity
+        right_force = 0.5 * target_velocity
+        back_force = 0.5 * target_velocity
 
-        # INSTRUCTIONS & HELP : 
-        #	-> Use the target velocity you calculated for the robot in previous task, and
-        #	Process it further to find what proportions of that effort should be given to 3 individuals wheels !!
-        #	Publish the calculated efforts to actuate robot by applying force vectors on provided topics
-        ############################################
-        pass
+        # Publish the calculated efforts to actuate the robot
+        left_msg = Twist()
+        left_msg.linear.x = left_force
+        self.left_wheel_force_pub.publish(left_msg)
 
+        right_msg = Twist()
+        right_msg.linear.x = right_force
+        self.right_wheel_force_pub.publish(right_msg)
+
+        back_msg = Twist()
+        back_msg.linear.x = back_force
+        self.back_wheel_force_pub.publish(back_msg)
+
+    def calculate_error(self, x_goal, y_goal, theta_goal):
+        # Assume you have access to the current robot pose (replace this with your actual pose retrieval logic)
+        current_pose = self.get_current_pose()
+
+        # Calculate the error based on the goal and current pose
+        error_x = x_goal - current_pose.x
+        error_y = y_goal - current_pose.y
+        error_theta = theta_goal - current_pose.theta
+
+        # Return the calculated error
+        return error_x, error_y, error_theta
+
+    def calculate_target_velocity(self, error):
+        # Assume proportional control for simplicity (replace this with your actual control algorithm)
+        proportional_gain = 0.1
+        target_velocity = proportional_gain * error
+
+        # Return the calculated target velocity
+        return target_velocity
+
+    def get_current_pose(self):
+        # Assume a simple way to get the current pose (replace this with your actual pose retrieval logic)
+        # For example, you may be subscribing to the Odometry topic
+        # Here, I'm creating a hypothetical Pose object with some values
+        current_pose = Pose(x=1.0, y=2.0, theta=0.5)
+        return current_pose
+
+    def main_loop(self):
+        while rclpy.ok():
+            if self.future.done():
+                try:
+                    response = self.future.result()
+                except Exception as e:
+                    self.get_logger().info('Service call failed %r' % (e,))
+                else:
+                    x_goal = response.x_goal
+                    y_goal = response.y_goal
+                    theta_goal = response.theta_goal
+                    self.flag = response.end_of_list
+
+                    # Calculate Error from feedback
+                    error = self.calculate_error(x_goal, y_goal, theta_goal)
+
+                    # Calculate the required velocity of the robot for the next iteration(s)
+                    target_velocity = self.calculate_target_velocity(error)
+
+                    # Apply inverse kinematics to get wheel forces
+                    self.inverse_kinematics(target_velocity)
+
+                    # Modify the condition to Switch to Next goal
+                    self.index += 1
+                    if self.flag == 1:
+                        self.index = 0
+                    self.send_request(self.index)
+
+            # Spin once to process callbacks
+            rclpy.spin_once(self)
+            self.rate.sleep()
 
 def main(args=None):
     rclpy.init(args=args)
     
-    # Create an instance of the HBController class
     hb_controller = HBController()
-   
-    # Send an initial request with the index from ebot_controller.index
     hb_controller.send_request(hb_controller.index)
-    
+
     # Main loop
-    while rclpy.ok():
+    hb_controller.main_loop()
 
-        # Check if the service call is done
-        if hb_controller.future.done():
-            try:
-                # response from the service call
-                response = hb_controller.future.result()
-            except Exception as e:
-                hb_controller.get_logger().infselfo(
-                    'Service call failed %r' % (e,))
-            else:
-                #########           GOAL POSE             #########
-                x_goal      = response.x_goal
-                y_goal      = response.y_goal
-                theta_goal  = response.theta_goal
-                hb_controller.flag = response.end_of_list
-                ####################################################
-                
-                # Calculate Error from feedback
-
-                # Change the frame by using Rotation Matrix (If you find it required)
-
-                # Calculate the required velocity of bot for the next iteration(s)
-                
-                # Find the required force vectors for individual wheels from it.(Inverse Kinematics)
-
-                # Apply appropriate force vectors
-
-                # Modify the condition to Switch to Next goal (given position in pixels instead of meters)
-                        
-                ############     DO NOT MODIFY THIS       #########
-                hb_controller.index += 1
-                if hb_controller.flag == 1 :
-                    hb_controller.index = 0
-                hb_controller.send_request(hb_controller.index)
-                ####################################################
-
-        # Spin once to process callbacks
-        rclpy.spin_once(hb_controller)
-    
     # Destroy the node and shut down ROS
     hb_controller.destroy_node()
     rclpy.shutdown()
 
-# Entry point of the script
 if __name__ == '__main__':
     main()
--- eYRC-2023_Hologlyph_Bots/hb_task2a/scripts/feedback.py
+++ eYRC-2023_Hologlyph_Bots/hb_task2a/scripts/feedback.py
@@ -1,48 +1,50 @@
-#! /usr/bin/env python3
-
-'''
-*****************************************************************************************
-*
-*        		===============================================
-*           		Hologlyph Bots (HB) Theme (eYRC 2023-24)
-*        		===============================================
-*
-*  This script is to implement Task 2A of Hologlyph Bots (HB) Theme (eYRC 2023-24).
-*  
-*  This software is made available on an "AS IS WHERE IS BASIS".
-*  Licensee/end user indemnifies and will keep e-Yantra indemnified from
-*  any and all claim(s) that emanate from the use of the Software or 
-*  breach of the terms of this agreement.
-*
-*****************************************************************************************
-'''
-################### IMPORT MODULES #######################
 import rclpy
 from rclpy.node import Node
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Pose2D
+from cv_bridge import CvBridge
+import cv2
 
-# Import the required modules
-##############################################################
-class ArUcoDetector(Node):
-
+class Feedback(Node):
     def __init__(self):
-        super().__init__('ar_uco_detector')
+        super().__init__('feedback')
+        self.subscription = self.create_subscription(Image, '/camera/image_raw', self.listener_callback, 10)
+        self.publisher_ = self.create_publisher(Pose2D, '/detected_aruco', 10)
+        self.cv_bridge = CvBridge()
 
-        # Subscribe the topic /camera/image_raw
+    def listener_callback(self, msg):
+        cv_image = self.convert_ros_to_cv(msg)
+        detected_pose = self.detect_aruco(cv_image)
+        self.publish_detected_pose(detected_pose)
 
+    def convert_ros_to_cv(self, ros_image_msg):
+        cv_image = self.cv_bridge.imgmsg_to_cv2(ros_image_msg, desired_encoding='bgr8')
+        return cv_image
 
-    def image_callback(self, msg):
-        #convert ROS image to opencv image
-        #Detect Aruco marker
-        # Publish the bot coordinates to the topic  /detected_aruco
+    def detect_aruco(self, cv_image):
+        aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
+        parameters = cv2.aruco.DetectorParameters()
+        corners, ids, _ = cv2.aruco.detectMarkers(cv_image, aruco_dict, parameters=parameters)
+
+        if ids is not None and len(ids) > 0:
+            x, y, theta = 0.0, 0.0, 0.0  # Replace with the actual values
+            detected_pose = Pose2D()
+            detected_pose.x = x
+            detected_pose.y = y
+            detected_pose.theta = theta
+            return detected_pose
+        else:
+            return None
+
+    def publish_detected_pose(self, pose):
+        if pose is not None:
+            self.publisher_.publish(pose)
 
 def main(args=None):
     rclpy.init(args=args)
-
-    aruco_detector = ArUcoDetector()
-
-    rclpy.spin(aruco_detector)
-
-    aruco_detector.destroy_node()
+    feedback = Feedback()
+    rclpy.spin(feedback)
+    feedback.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
