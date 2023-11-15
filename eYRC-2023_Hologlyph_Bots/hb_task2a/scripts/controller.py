f45eee0e221a1db510ba84bc8c4c30466f1b60a0
Sprawl2387 <scraggly5717@gmail.com>
2023-11-15 22:43:26 +0530

    working code

--- eYRC-2023_Hologlyph_Bots/hb_task2a/scripts/controller.py
+++ eYRC-2023_Hologlyph_Bots/hb_task2a/scripts/controller.py
@@ -1,153 +1,111 @@
-#! /usr/bin/env python3
-
-'''
-*****************************************************************************************
-*
-*        		===============================================
-*           		Hologlyph Bots (HB) Theme (eYRC 2023-24)
-*        		===============================================
-*
-*  This script is to implement Task 2A of Hologlyph Bots (HB) Theme (eYRC 2023-24).
-*  
-*  This software is made available on an "AS IS WHERE IS BASIS".
-*  Licensee/end user indemnifies and will keep e-Yantra indemnified from
-*  any and all claim(s) that emanate from the use of the Software or 
-*  breach of the terms of this agreement.
-*
-*****************************************************************************************
-'''
-
-
-# Team ID:		[ 3695 ]
-# Author List:		[ Names of team members worked on this file separated by Comma: Name1, Name2, ... ]
-# Filename:		feedback.py
-# Functions:
-#			[ Comma separated list of functions in this file ]
-# Nodes:		Add your publishing and subscribing node
-
-#! /usr/bin/env python3
-
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Twist
-from nav_msgs.msg import Odometry
-import time
-import math
-from tf_transformations import euler_from_quaternion
-from my_robot_interfaces.srv import NextGoal
-
-class Pose:
-    def __init__(self, x, y, theta):
-        self.x = x
-        self.y = y
-        self.theta = theta
+import math as m
+from my_robot_interfaces.srv import NextGoal   
+from geometry_msgs.msg import Pose2D  
+from geometry_msgs.msg import Wrench      
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2   
 
 class HBController(Node):
     def __init__(self):
-        super().__init__('hb_controller')
-
-        self.left_wheel_force_pub = self.create_publisher(Twist, '/hb_bot_1/left_wheel_force', 10)
-        self.right_wheel_force_pub = self.create_publisher(Twist, '/hb_bot_1/right_wheel_force', 10)
-        self.back_wheel_force_pub = self.create_publisher(Twist, '/hb_bot_1/back_wheel_force', 10)
-
-        self.rate = self.create_rate(100)
+        super().__init__('bot')
+        self.subscriber = self.create_subscription(Pose2D, '/detected_aruco', self.pose_cb, 10)
+        self.frame_subscriber = self.create_subscription(Image, '/camera/image_raw', self.image_cb, 10)
+        self.l_wheel_publisher = self.create_publisher(Wrench, '/hb_bot_1/left_wheel_force', 10)
+        self.r_wheel_publisher = self.create_publisher(Wrench, '/hb_bot_1/right_wheel_force', 10)
+        self.b_wheel_publisher = self.create_publisher(Wrench, '/hb_bot_1/rear_wheel_force', 10)
+        self.l_wheel = Wrench()
+        self.r_wheel = Wrench()
+        self.b_wheel = Wrench()
+        self.rate = self.create_rate(25)
+        self.hb_x = 0
+        self.hb_y = 0
+        self.hb_theta = 0
+        self.x_goal = 0
+        self.y_goal = 0
+        self.theta_goal = 0
+        self.proportional = [5 , 5, 30.0]
         self.cli = self.create_client(NextGoal, 'next_goal')      
         self.req = NextGoal.Request() 
         self.index = 0
-
-    def send_request(self, request_goal):
-        self.req.request_goal = request_goal
+        self.future = None
+        self.cv_bridge = CvBridge()
+        
+    def send_request(self):
+        self.req.request_goal = self.index
         self.future = self.cli.call_async(self.req)
+        
+    def pose_cb(self, msg):
+        self.hb_x = msg.x
+        self.hb_y = msg.y
+        self.hb_theta = msg.theta
+    
+    def rtn_mtrx(self, theta):
+        return [[m.cos(theta), -m.sin(theta)],
+                [m.sin(theta), m.cos(theta)]]
 
-    def inverse_kinematics(self, target_velocity):
-        # Assume a simple proportional control for inverse kinematics
-        left_force = 0.5 * target_velocity
-        right_force = 0.5 * target_velocity
-        back_force = 0.5 * target_velocity
+    def inverse_kinematics(self, vx, vy, w):
+        self.l_wheel.force.y = -vx - vy + w
+        self.r_wheel.force.y = -vx + vy + w
+        self.b_wheel.force.y = vx + w
 
-        # Publish the calculated efforts to actuate the robot
-        left_msg = Twist()
-        left_msg.linear.x = left_force
-        self.left_wheel_force_pub.publish(left_msg)
+    def image_cb(self, msg):
+        global pt_lst
+        self.cv_img = self.cv_bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+        for pt in pt_lst:
+            cv2.circle(self.cv_img, pt, 2, (0,255,255), -1)
+        cv2.putText(self.cv_img, 'Angle = ' + str(self.hb_theta),
+            (20,40), cv2.FONT_HERSHEY_SIMPLEX,
+            1, (255, 0, 255), 2)
+        cv2.circle(self.cv_img, (int(self.hb_x), int(self.hb_y)), 2, (0,0,255), -1)
+        cv2.putText(self.cv_img, '(' + str(self.hb_x) + ',' + str(self.hb_y) + ')',
+            (int(self.hb_x), int(self.hb_y)), cv2.FONT_HERSHEY_SIMPLEX,
+            0.7, (255, 0, 0), 2)
+        cv2.imshow('img', self.cv_img)
+        cv2.waitKey(1)
 
-        right_msg = Twist()
-        right_msg.linear.x = right_force
-        self.right_wheel_force_pub.publish(right_msg)
-
-        back_msg = Twist()
-        back_msg.linear.x = back_force
-        self.back_wheel_force_pub.publish(back_msg)
-
-    def calculate_error(self, x_goal, y_goal, theta_goal):
-        # Assume you have access to the current robot pose (replace this with your actual pose retrieval logic)
-        current_pose = self.get_current_pose()
-
-        # Calculate the error based on the goal and current pose
-        error_x = x_goal - current_pose.x
-        error_y = y_goal - current_pose.y
-        error_theta = theta_goal - current_pose.theta
-
-        # Return the calculated error
-        return error_x, error_y, error_theta
-
-    def calculate_target_velocity(self, error):
-        # Assume proportional control for simplicity (replace this with your actual control algorithm)
-        proportional_gain = 0.1
-        target_velocity = proportional_gain * error
-
-        # Return the calculated target velocity
-        return target_velocity
-
-    def get_current_pose(self):
-        # Assume a simple way to get the current pose (replace this with your actual pose retrieval logic)
-        # For example, you may be subscribing to the Odometry topic
-        # Here, I'm creating a hypothetical Pose object with some values
-        current_pose = Pose(x=1.0, y=2.0, theta=0.5)
-        return current_pose
-
-    def main_loop(self):
-        while rclpy.ok():
-            if self.future.done():
-                try:
-                    response = self.future.result()
-                except Exception as e:
-                    self.get_logger().info('Service call failed %r' % (e,))
-                else:
-                    x_goal = response.x_goal
-                    y_goal = response.y_goal
-                    theta_goal = response.theta_goal
-                    self.flag = response.end_of_list
-
-                    # Calculate Error from feedback
-                    error = self.calculate_error(x_goal, y_goal, theta_goal)
-
-                    # Calculate the required velocity of the robot for the next iteration(s)
-                    target_velocity = self.calculate_target_velocity(error)
-
-                    # Apply inverse kinematics to get wheel forces
-                    self.inverse_kinematics(target_velocity)
-
-                    # Modify the condition to Switch to Next goal
-                    self.index += 1
-                    if self.flag == 1:
-                        self.index = 0
-                    self.send_request(self.index)
-
-            # Spin once to process callbacks
-            rclpy.spin_once(self)
-            self.rate.sleep()
+pt_lst = []
 
 def main(args=None):
-    rclpy.init(args=args)
-    
-    hb_controller = HBController()
-    hb_controller.send_request(hb_controller.index)
-
-    # Main loop
-    hb_controller.main_loop()
-
-    # Destroy the node and shut down ROS
-    hb_controller.destroy_node()
+    global pt_lst
+    rclpy.init(args=args)    
+    bot = HBController()   
+    bot.send_request()    
+    while rclpy.ok():
+        if bot.future.done():
+            try:
+                resp = bot.future.result()
+            except Exception as e:
+                bot.get_logger().infselfo(
+                    'Service call failed %r' % (e,))
+            else:
+                bot.x_goal      = resp.x_goal + 250
+                bot.y_goal      = resp.y_goal + 250
+                bot.theta_goal  = resp.theta_goal
+                bot.flag = resp.end_of_list
+                gf_x_err = bot.x_goal - bot.hb_x
+                gf_y_err = -(bot.y_goal - bot.hb_y)   
+                gf_theta_err = bot.theta_goal - bot.hb_theta
+                rot_mtx = bot.rtn_mtrx(bot.hb_theta)
+                bf_x_err = rot_mtx[0][0]*gf_x_err + rot_mtx[0][1]*gf_y_err
+                bf_y_err = rot_mtx[1][0]*gf_x_err + rot_mtx[1][1]*gf_y_err
+                vx = bot.proportional[0]*bf_x_err
+                vy = bot.proportional[1]*bf_y_err
+                w = bot.proportional[2]*gf_theta_err
+                bot.inverse_kinematics(vx, vy, w)
+                bot.l_wheel_publisher.publish(bot.l_wheel)
+                bot.r_wheel_publisher.publish(bot.r_wheel)
+                bot.b_wheel_publisher.publish(bot.b_wheel)
+                if abs(bf_x_err) < 1 and abs(bf_y_err) < 1 :        
+                    pt_lst.append((int(bot.hb_x), int(bot.hb_y)))
+                    bot.index += 1
+                    if bot.flag == 1 :
+                        bot.index = 0
+                    bot.send_request()
+        rclpy.spin_once(bot)
+    bot.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
--- eYRC-2023_Hologlyph_Bots/hb_task2a/scripts/feedback.py
+++ eYRC-2023_Hologlyph_Bots/hb_task2a/scripts/feedback.py
@@ -1,51 +1,96 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
-from geometry_msgs.msg import Pose2D
 from cv_bridge import CvBridge
 import cv2
-
-class Feedback(Node):
+import math as m 
+from geometry_msgs.msg import Pose2D
+class ArUcoDetector(Node):
     def __init__(self):
-        super().__init__('feedback')
-        self.subscription = self.create_subscription(Image, '/camera/image_raw', self.listener_callback, 10)
-        self.publisher_ = self.create_publisher(Pose2D, '/detected_aruco', 10)
+        super().__init__('ar_uco_detector')
+        self.sub = self.create_subscription(Image, '/camera/image_raw', self.image_callback, 10)
+        self.pub = self.create_publisher(Pose2D, '/detected_aruco', 10)
+        self.pose = Pose2D()
         self.cv_bridge = CvBridge()
+        self.dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
+        self.param1 =  cv2.aruco.DetectorParameters()
+        self.param1.adaptiveThreshWinSizeMin = 3
+        self.param1.adaptiveThreshWinSizeMax = 13
+        self.param1.adaptiveThreshWinSizeStep = 2
+        self.param1.adaptiveThreshConstant = 13
+        self.param2 = cv2.aruco.DetectorParameters()
+        self.hb_x = 0.0
+        self.hb_y = 0.0
+        self.hb_theta = 0.0
 
-    def listener_callback(self, msg):
-        cv_image = self.convert_ros_to_cv(msg)
-        detected_pose = self.detect_aruco(cv_image)
-        self.publish_detected_pose(detected_pose)
+    def create_perspective_image(self, img):
+        detect = cv2.aruco.ArucoDetector(self.dictionary, self.param1)
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        corners, ids, _ = detect.detectMarkers(gray_img)
+        if ids is not None and len(ids) == 5:
+            cv2.aruco.drawDetectedMarkers(img, corners, ids)
+            ids = ids.flatten().tolist()
+            center_lst = []
+            for index in range(len(ids)):
+                aruco_x = int((corners[index][0][0][0] + corners[index][0][1][0] + corners[index][0][2][0] + corners[index][0][3][0])/4)
+                aruco_y = int((corners[index][0][0][1] + corners[index][0][1][1] + corners[index][0][2][1] + corners[index][0][3][1])/4)
+                center_lst.append((aruco_x, aruco_y))
+            ul_index = ids.index(8)
+            ur_index = ids.index(10)
+            lr_index = ids.index(12)
+            ll_index = ids.index(4)
+            width_1 = m.sqrt((center_lst[ul_index][0] - center_lst[ll_index][0])**2 + (center_lst[ul_index][1] - center_lst[ll_index][1])**2)
+            width_2 = m.sqrt((center_lst[ur_index][0] - center_lst[lr_index][0])**2 + (center_lst[ur_index][1] - center_lst[lr_index][1])**2)
+            print(f'width_1: {width_1}')
+            print(f'width_2: {width_2}')
+        cv2.imshow('d', img)
+        
+    def image_callback(self, msg):
+        cv_img = self.cv_bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+        cv_img_zoomed = cv2.resize(cv_img, (2000,2000))
+        gray_img = cv2.cvtColor(cv_img_zoomed, cv2.COLOR_BGR2GRAY)
+        detector = cv2.aruco.ArucoDetector(self.dictionary, self.param2)
+        corners, ids, _ = detector.detectMarkers(gray_img)
+        if ids is not None:
+            ids = ids.flatten().tolist()
+            if 1 in ids:
+                robot_corners = corners[ids.index(1)]
+                robot_corners = robot_corners.reshape((4, 2))
+                (tl, tr, br, bl) = robot_corners
+                p_x = int((tr[0] + br[0]) / 2.0)
+                p_y = int((tr[1] + br[1]) / 2.0)
+                c_x = int((tl[0] + br[0]) / 2.0)
+                c_y = int((tl[1] + br[1]) / 2.0)
+                self.hb_x = c_x/4
+                self.hb_y = c_y/4
+                if c_x == p_x:
+                    if p_y-c_y < 0:
+                        self.hb_theta = 0.5*m.pi
+                    else:
+                        self.hb_theta = -0.5*m.pi
+                else:
+                    if p_y-c_y < 0:
+                        if p_x > c_x:
+                            self.hb_theta = m.atan(abs(p_y-c_y)/abs(p_x-c_x))
+                        else:
+                            self.hb_theta = m.pi - m.atan(abs(p_y-c_y)/abs(p_x-c_x))
+                    else:
+                        if p_x > c_x:
+                            self.hb_theta = -m.atan(abs(p_y-c_y)/abs(p_x-c_x))
+                        else:
+                            self.hb_theta = -(m.pi - m.atan(abs(p_y-c_y)/abs(p_x-c_x)))
+                self.hb_theta = round(self.hb_theta + 0.09, 6)
 
-    def convert_ros_to_cv(self, ros_image_msg):
-        cv_image = self.cv_bridge.imgmsg_to_cv2(ros_image_msg, desired_encoding='bgr8')
-        return cv_image
-
-    def detect_aruco(self, cv_image):
-        aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
-        parameters = cv2.aruco.DetectorParameters()
-        corners, ids, _ = cv2.aruco.detectMarkers(cv_image, aruco_dict, parameters=parameters)
-
-        if ids is not None and len(ids) > 0:
-            x, y, theta = 0.0, 0.0, 0.0  # Replace with the actual values
-            detected_pose = Pose2D()
-            detected_pose.x = x
-            detected_pose.y = y
-            detected_pose.theta = theta
-            return detected_pose
-        else:
-            return None
-
-    def publish_detected_pose(self, pose):
-        if pose is not None:
-            self.publisher_.publish(pose)
-
+        self.pose.x = self.hb_x
+        self.pose.y = self.hb_y
+        self.pose.theta = self.hb_theta
+        self.pub.publish(self.pose)
+        
 def main(args=None):
     rclpy.init(args=args)
-    feedback = Feedback()
-    rclpy.spin(feedback)
-    feedback.destroy_node()
+    aruco_detect = ArUcoDetector()
+    rclpy.spin(aruco_detect)
+    aruco_detect.destroy_node()
     rclpy.shutdown()
-
 if __name__ == '__main__':
     main()
